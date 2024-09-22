4ba8f430845071bf30fbebad582e9f88613797f8
Linh <quanglinh1286@gmail.com>
2024-09-22 14:39:42 +0700

    ChangePerectSecond

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
